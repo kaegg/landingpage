9c0fb54b0efb49075ae335e49f3e9186dd9fef4c
Kauan <kauan.eguchi@hotmail.com>
2023-05-02 13:24:58 -0300

    animation/footer

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
+// $(document).ready(function() {
+//   function bounce() {
+//     $('.bouncing').animate({
+//       top: '+=5px',
+//       bottom: '-=5px'
+//     }, 1000, function() {
+//       $('.bouncing').animate({
+//         top: '-=5px',
+//         bottom: '+=5px'
+//       }, 1000, bounce);
+//     });
+//   }
+
+//   bounce();
+// });
+
+
 $(document).ready(function() {
+
   $('.circle2').animate({
-    left: '+=100px',
-    top: '+=100px'
-  }, 1000);
-});
+    left:'-50%'
+  },1000),
+
+  $('.circle4').animate({
+    left:'70%'
+  },1000)
+
+  $('.circle5').animate({
+    left:'-10%'
+  },1000)
+
+})
